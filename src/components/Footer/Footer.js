126226b803be0582d4fa28d7e2a625090760a19e
Zakhar Kibanov <z.kibanov@yandex.ru>
2021-01-02 18:24:15 +0300

    version before linters

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,16 +1,44 @@
 import React,{Component} from 'react';
 import TasksFilter from '../TasksFilter';
+import PropTypes from 'prop-types';
 
 export default class Footer extends Component {
 	
 	render() {
-		const {count} = this.props
+		const {count,clearCompleted,filterTasks} = this.props;
 
 		return (
+
         <footer className = 'footer'>
         <span className = 'todo-count'>{count} items left</span>
-        <TasksFilter />
-        <button className="clear-completed">Clear completed</button>
+        <TasksFilter
+        filterTasks = {(e)=>{
+			const elem = e.target
+			const siblings = Array.from(elem.parentNode.parentNode.querySelectorAll(`button`));
+			siblings.forEach(el => el.className = '');
+			e.target.className = 'selected';
+			filterTasks(e.target)
+			}}
+        />
+        <button className="clear-completed"
+        onClick = {clearCompleted}
+        >Clear completed</button>
         </footer>
     )}
-	}
+
+    static defaultProps = {
+        count: 0,
+        filterTasks: () => {console.log('We have lost function filterTasks somewhere in import')},
+        clearCompleted: ()=>{console.log('We have lost function clearCompleted somewhere in import')}     
+    }
+
+    static propTypes = {
+        count:PropTypes.number,
+        filterTasks: PropTypes.func,
+        clearCompleted: PropTypes.func
+    }
+    }
+    
+ 
+
+
--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import NewTaskForm from './components/NewTaskForm';
 import TaskList from './components/TaskList';
 import Footer from './components/Footer';
 
-const count = 1;
 let elId = 100;
 
 class App extends Component {
@@ -12,51 +11,108 @@ class App extends Component {
 		super();
 		this.state = {
 		tasks:[
-    {description:'Completed task',status:'completed',created:new Date(2020, 12, 24),id:1},
+    {description:'Completed task',completed:true,created:new Date(2020, 12, 24),id:1},
     {description:'Editing task',completed:false,created:new Date(2020, 12, 11),id:2},
     {description:'Active task',completed:false,created:new Date(2020, 10, 10),id:3}
-    ]
+    ],
+    filterOption:'all'
+		}	
+		};
+		
+	findId = (arr,id) => arr.findIndex((el)=>el.id===id)
+	
+	generateTaskObject = (descriptionText,completedStatus=false,createdDate,idNum)=>{
+		return {
+			description:descriptionText,
+			completed:completedStatus,
+			created:createdDate,
+			id:idNum
+			}
 		}
+	
+	markAsDone = (id) => {
+		this.setState(({tasks})=>{
+			const idx = this.findId(tasks,id)
+	        const res = [...tasks]
+			res[idx].completed = !tasks[idx].completed
+			return res
+		})
 		};
 		
 	deleteItem = (id) => {
 		this.setState (({tasks})=>{
-			const idx = tasks.findIndex((el)=> el.id === id)
+			const idx = this.findId(tasks,id)
 			const before = tasks.slice(0,idx);
 			const after = tasks.slice(idx+1);
 			return {tasks:[...before,...after]}
 		})
 	};
 	
-	addItem = (e) => {
-		if (e.code === 'Enter'){
+	addItem = (text) => {
 			this.setState(({tasks})=>{
-				const text = e.target.value;
-				const newItemId = elId++;
-				const newItem = [...tasks,{
-					description:text,
-					created:Date.now(),
-					id:newItemId
-					}]
-                console.log(newItem)
-                return {tasks:newItem}
+			const newItemId = elId++;
+			const newItem = this.generateTaskObject(text,false,Date.now(),newItemId)
+			const newTasks = [...tasks,newItem]
+            return {tasks:newTasks}
 				})
-				}
 			}
-
+			
+	renameItem = (text,id) => {
+		this.setState (({tasks})=>{
+			const idx = this.findId(tasks,id)
+			const before = tasks.slice(0,idx);
+			const after = tasks.slice(idx+1);
+			const newItem = this.generateTaskObject(text,false,Date.now(),id)
+			return {tasks:[...before,newItem,...after]}
+		})
+		}
+      
+     removeAllCompleted = (status) => {
+		 this.setState(({tasks})=>{
+			 const res = this.state.tasks.filter((el)=> el.completed === false) 
+			 return {tasks:res}
+		 })
+		 }
+		 
+	filterTasks = (filter) => {
+		const filterOpt = filter.dataset['f'];
+		this.setState(({filterOption}) => {
+		return {filterOption:filterOpt}
+	})
+	}
+	
+	applyFilter = () => {
+		const filterOpt = this.state.filterOption;
+		const res = this.state.tasks;
+		switch (filterOpt) {
+			case 'active':
+			return (res.filter((el)=>el.completed === false));
+			case 'completed':
+			return (res.filter((el)=>el.completed === true));
+			default:
+			return res;
+ 			}
+		}
 	
 	render () {
+		const count = this.state.tasks.filter((el)=>!el.completed).length;
 		return (
         <section className = 'todoapp'>
           <NewTaskForm 
-          onItemAdded = {this.addItem}
-          addTaskOnEnterKeydown  = { (text) => this.addItem(text) }
+          onItemAdded = {(text) => this.addItem(text)}
           />
           <section className = 'main'>
-             <TaskList tasks = {this.state.tasks}
-             onDeleted = { (id) => this.deleteItem(id)}
+             <TaskList 
+             tasks = {this.applyFilter()}
+             onDeleted = { this.deleteItem}
+             onDone = { this.markAsDone}
+             onRename = { this.renameItem }             
               />
-             <Footer count = {count} />
+             <Footer 
+             filterTasks = {this.filterTasks}
+             count = {count} 
+             clearCompleted={this.removeAllCompleted}
+             />
            </section>
         </section>
     )}
