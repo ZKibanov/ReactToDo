a34b69ed3d12e5d34a29495f82140fed1ddc3a29
Zakhar Kibanov <z.kibanov@yandex.ru>
2021-01-09 11:03:09 +0300

    added prettier

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,42 +1,37 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import TasksFilter from '../TasksFilter'
+import TasksFilter from '../TasksFilter';
 
- const Footer = (props) => {
+const Footer = (props) => {
   Footer.defaultProps = {
     count: 0,
-    filterTasks: () => { console.log('We have lost function filterTasks somewhere in import'); },
-    clearCompleted: () => { console.log('We have lost function clearCompleted somewhere in import'); },
+    filterTasks: () => {
+      console.log('We have lost function filterTasks somewhere in import');
+    },
+    clearCompleted: () => {
+      console.log('We have lost function clearCompleted somewhere in import');
+    },
     filterOption: 'all',
-  }
-  
+  };
+
   Footer.propTypes = {
     count: PropTypes.number,
     filterTasks: PropTypes.func,
     clearCompleted: PropTypes.func,
     filterOption: 'all',
-  }
+  };
 
-    const { count, clearCompleted, filterTasks,filterOption } = props;
+  const { count, clearCompleted, filterTasks, filterOption } = props;
 
-    return (
-
-        <footer className = 'footer'>
-        <span className = 'todo-count'>{count} items left</span>
-        <TasksFilter
-        filterTasks = {filterTasks}
-        filterOption = {filterOption}
-        />
-        <button type = "button" className="clear-completed"
-        onClick = {clearCompleted}
-        >Clear completed</button>
-        </footer>
-    );
-
-}
-
-
-
-
+  return (
+    <footer className="footer">
+      <span className="todo-count">{count} items left</span>
+      <TasksFilter filterTasks={filterTasks} filterOption={filterOption} />
+      <button type="button" className="clear-completed" onClick={clearCompleted}>
+        Clear completed
+      </button>
+    </footer>
+  );
+};
 
 export default Footer;
--- src/components/Task/Task.js
+++ src/components/Task/Task.js
@@ -3,31 +3,34 @@ import formatDistanceToNow from 'date-fns/formatDistanceToNow';
 import PropTypes from 'prop-types';
 
 export default class Task extends Component {
-
-	static defaultProps = {
-	  status: '',
+  static defaultProps = {
+    status: '',
     created: Date.now(),
     description: 'this task is missing',
     completed: false,
-    onRename: () => { console.log('We have lost function onRename somewhere in import'); 
-  },
-    onDeleted: () => { console.log('We have lost function onDeleted somewhere in import'); 
-  },
-    onDone:() => { console.log('We have lost function onDone somewhere in import'); 
-  },
-}
+    onRename: () => {
+      console.log('We have lost function onRename somewhere in import');
+    },
+    onDeleted: () => {
+      console.log('We have lost function onDeleted somewhere in import');
+    },
+    onDone: () => {
+      console.log('We have lost function onDone somewhere in import');
+    },
+  };
 
-	static propTypes = {
- 	  description: PropTypes.string,
+  static propTypes = {
+    description: PropTypes.string,
     completed: PropTypes.bool,
     status: PropTypes.string,
     created: PropTypes.instanceOf(Date),
     onRename: PropTypes.func,
     onDeleted: PropTypes.func,
     onDone: PropTypes.func,
-  }
-  
-  constructor() { // чтоб не биндить this на функцию в рендере
+  };
+
+  constructor() {
+    // чтоб не биндить this на функцию в рендере
     super();
     this.state = {
       editing: false,
@@ -38,53 +41,40 @@ export default class Task extends Component {
     };
   }
 
-	onSubmit =(ev) => {
-    const {onRename} = this.props;
-	  ev.preventDefault();
-	  onRename(ev.target.lastChild.value);
-	  this.setState({ editing: false });
-	}
+  onSubmit = (ev) => {
+    const { onRename } = this.props;
+    ev.preventDefault();
+    onRename(ev.target.lastChild.value);
+    this.setState({ editing: false });
+  };
 
-	render() {
-	  let {
-	    status
-    } = this.props;
-    const {description, created, onDeleted, onDone, completed} = this.props;
-	  const { editing } = this.state;
-	  if (completed) {
-	    status += 'completed';
-	  }
+  render() {
+    let { status } = this.props;
+    const { description, created, onDeleted, onDone, completed } = this.props;
+    const { editing } = this.state;
+    if (completed) {
+      status += 'completed';
+    }
 
-	  if (editing) {
-	    status += 'editing';
-	  }
+    if (editing) {
+      status += 'editing';
+    }
 
-	  return (
-    <li className={status}>
-        <div className = 'view'>
-         <input className = 'toggle'
-         type="checkbox"
-         defaultChecked = {completed}
-         onClick = {onDone} />
-         <label>
-         <span className = 'description'
-         >{description}</span>
-         <span className = 'created'>{formatDistanceToNow(
-           created,
-           { includeSeconds: true },
-         )}</span>
-         </label>
-         <button aria-label="edit" type = "button" className = 'icon icon-edit'
-         onClick = {this.turnToEdit}
-          />
-         <button aria-label="delete" type = "button" className = 'icon icon-destroy'
-         onClick = {onDeleted}
-          />
+    return (
+      <li className={status}>
+        <div className="view">
+          <input className="toggle" type="checkbox" defaultChecked={completed} onClick={onDone} />
+          <label>
+            <span className="description">{description}</span>
+            <span className="created">{formatDistanceToNow(created, { includeSeconds: true })}</span>
+          </label>
+          <button aria-label="edit" type="button" className="icon icon-edit" onClick={this.turnToEdit} />
+          <button aria-label="delete" type="button" className="icon icon-destroy" onClick={onDeleted} />
         </div>
-         <form
-         onSubmit = {this.onSubmit}
-         ><input type = 'text' className = 'edit' /></form>
-    </li>);
-	}
-
+        <form onSubmit={this.onSubmit}>
+          <input type="text" className="edit" />
+        </form>
+      </li>
+    );
+  }
 }
